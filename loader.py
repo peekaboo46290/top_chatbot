418f6f1f225905c501cee77f95a5380548751ccf
fester46290 <peekaboo46290@gmail.com>
2025-12-10 14:38:15 +0100

    starting

---
+++ loader.py
@@ -0,0 +1,14 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv(".env")
+
+url = os.getenv("NEO4J_URI")
+username = os.getenv("NEO4J_USERNAME")
+password = os.getenv("NEO4J_PASSWORD")
+ollama_base_url = os.getenv("OLLAMA_BASE_URL")
+embedding_model_name = os.getenv("EMBEDDING_MODEL")
+
+print(password)
